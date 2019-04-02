5f2798ccffb2dac66ee82709ad9b9a55f457d05c
ninlgde <ninlgde.ma@gmail.com>
2019-04-02 23:50:25 +0800

    api test

--- api_request.go
+++ api_request.go
@@ -1,36 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"sync"
+	"time"
 )
 
-func httpGet() {
-	resp, err := http.Get("http://localhost:8888/pi/10000")
-	if err != nil {
-		// handle error
-	}
-
-	defer resp.Body.Close()
-	//body, err := ioutil.ReadAll(resp.Body)
-	//if err != nil {
-	//	handle error
-	//}
-
-	//fmt.Println(string(body))
+func httpGet(version string) {
+	http.Get("http://localhost:8888/" + version + "/pi/10000")
 }
 
-func main() {
+func TestGet(version string) {
 	var wg sync.WaitGroup
 
 	wg.Add(1000)
 
+	t1 := time.Now()
 	for i := 0; i < 1000; i++ {
 		go func() {
-			httpGet()
+			httpGet(version)
 			wg.Done()
 		}()
 	}
 
 	wg.Wait()
+	elapsed := time.Since(t1)
+	fmt.Println(version, " use ", elapsed)
+}
+
+func main() {
+	//TestGet("v3")
+	//TestGet("vgrpc")
+	TestGet("v4")
 }
